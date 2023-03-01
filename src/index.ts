a17caf55a749866bb697a7b3bfdc340f773f89c9
Leotor1157 <leo.tor1157@gmail.com>
2023-03-01 10:06:38 +0100

    commit 1/3

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,15 @@
-console.log('bonjour')
+import fastify from "fastify"
+
+const newApp = fastify()
+
+newApp.get('/', () => 'Bienvenue sur mon serveur')
+
+newApp.get('/hello', () => {
+  console.log('Bonjour tout le monde !')
+
+  return 'Bonjour tout le monde'
+})
+
+newApp.listen({ port: 4646, host: '127.0.0.1' }, () => {
+  console.log('Mon serveur est prèt : http://127.0.0.1:4646')
+})
